e1e780b6ad4589e739cc7f355b8b3f791b822b6f
Pantsworth <nski6m@gmail.com>
2016-02-17 00:57:29 -0600

    added mouth detection and improved eye detection by reducing search area.

--- test_blinkDetection.py
+++ test_blinkDetection.py
@@ -3,12 +3,12 @@ import cv2
 import time
 
 cap = cv2.VideoCapture(0)
-cap.set(cv2.cv.CV_CAP_PROP_FPS,30)
-
+cap.set(cv2.cv.CV_CAP_PROP_FPS, 30)
 
 eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
 handCascade = cv2.CascadeClassifier("haarcascade_hand.xml")
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+mouth_cascade = cv2.CascadeClassifier("haarcascade_mcs_mouth.xml")
 
 blinkBool = False
 blinkCount = 0
@@ -19,65 +19,69 @@ start = 0
 
 blinkFrequency = 0.0
 
-while(True):
-	ret,img = cap.read()
-	blinkFrequency = 60*blinkCount/((time.clock() - overallTime))
-	# while(True):
-		# Capture frame-by-frame
-	# ret, img = cap.read()
-	# img = cv2.imread('family.jpg')
-	img = cv2.resize(img,(0,0),fx=0.4,fy=0.4)
-	# Our operations on the frame come here
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+while (True):
+    ret, img = cap.read()
+    blinkFrequency = 60 * blinkCount / ((time.clock() - overallTime))
+    # while(True):
+    # Capture frame-by-frame
+    # ret, img = cap.read()
+    # img = cv2.imread('family.jpg')
+    img = cv2.resize(img, (0, 0), fx=0.4, fy=0.4)
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    faces = face_cascade.detectMultiScale(gray, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20, 20))
 
-	faces = face_cascade.detectMultiScale(gray, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
+    flag = False
 
-	flag = False
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + (3*h/4), x:x + w]
+        roi_color = img[y:y + (3*h/4), x:x + w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
-	for (x,y,w,h) in faces:
-	    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-	    roi_gray = gray[y:y+h, x:x+w]
-	    roi_color = img[y:y+h, x:x+w]
-	    eyes = eye_cascade.detectMultiScale(roi_gray)
-	    for (ex,ey,ew,eh) in eyes:
-	        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-	    if len(eyes)==0:
-	    	flag = True
-	    #limit to 1
-	    break
+        if len(eyes) == 0:
+            flag = True
+            # limit to 1
 
+        roi_gray_mouth = gray[(y+h/2):y+h, x:x + w]
+        roi_color_mouth = img[(y+h/2):y+h, x:x + w]
 
-	if (time.clock() - start) > 0.4:
-		if (flag==False):
-			blinkBool = False
-	#now, if flag is true, then figure out blinking
-	if (flag==True):
-		if (blinkBool==False):
-			#start timer
-			start = time.clock()
-			blinkBool = True
-			blinkCount+=1
-			print "Detect blink, total:",blinkCount
-			print "Blink Frequency: ",blinkFrequency," in ",(time.clock()-overallTime)," sec"
-		else:
-			print "detect blink but disabled"
-		#get the time spent
+        mouth = mouth_cascade.detectMultiScale(roi_gray_mouth)
 
-		#if less than 20 ms, do nothing
+        for (mx, my, mw, mh) in mouth:
+            cv2.rectangle(roi_color_mouth, (mx, my), (mx + mw, my + mh), (255, 0, 255), 2)
+        break
 
+    if (time.clock() - start) > 0.4:
+        if (flag == False):
+            blinkBool = False
+        # now, if flag is true, then figure out blinking
+    if (flag == True):
+        if (blinkBool == False):
+            # start timer
+            start = time.clock()
+            blinkBool = True
+            blinkCount += 1
+            print "Detect blink, total:", blinkCount
+            print "Blink Frequency: ", blinkFrequency, " in ", (time.clock() - overallTime), " sec"
+        else:
+            print "detect blink but disabled"
+        # get the time spent
 
+        # if less than 20 ms, do nothing
 
-	# print "Found ",len(eyes)," eyes!"
-	# print "Found ",len(eyes)," eyes!"
-	# print "Found ",len(faces)," faces!"
-	# Display the resulting frame
-	cv2.imshow('Video',img)
-	if (blinkCount==50):
-		break
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-	    break
-# time.sleep(1)
+    # print "Found ",len(eyes)," eyes!"
+    # print "Found ",len(eyes)," eyes!"
+    # print "Found ",len(faces)," faces!"
+    # Display the resulting frame
+    cv2.imshow('Video', img)
+    if (blinkCount == 50):
+        break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break  # time.sleep(1)
 # cv2.waitKey(0)	
 
 # When everything done, release the capture
