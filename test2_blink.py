50f3dc6d2370e9c1fda56563ec1f0dd0dac16aca
Bruno Peynetti <bpeynetti@u.northwestern.edu>
2016-03-07 21:19:46 -0600

    work on data collection

--- test2_blink.py
+++ test2_blink.py
@@ -6,13 +6,23 @@ import urllib
 import urllib2
 import sys
 
+file = open('not_sleepy1.csv','w')
+file.write("Blink_Ratio,Blink_Length\n")
 # from matplotlib import pyplot as plt
+def getRate(timeArray):
+
+
+    now = time.clock()
+    only_past_minute = [x for x in timeArray if x>(now-60)]
+    print only_past_minute
+    
+    return len(only_past_minute)
 
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.cv.CV_CAP_PROP_FPS, 20)
 
-eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+eyes = cv2.CascadeClassifier("haarcascade_eye.xml")
 handCascade = cv2.CascadeClassifier("haarcascade_hand.xml")
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 mouth_cascade = cv2.CascadeClassifier("haarcascade_mcs_mouth.xml")
@@ -38,16 +48,17 @@ yawn_start = 0
 overallTime = time.clock()
 timeToSend = time.clock()
 sent = False
+recorded_time_blink=False
+
+blinkArr = []
+blinkLengthArr = []
+lastBlink = time.clock() #set to start at 0
+
+yawnTimes = []
+txt = ""
 
 while (True):
     ret, img = cap.read()
-    blink_frequency = 60 * blink_count / ((time.clock() - overallTime))
-    yawn_frequency = 60 * yawn_count / ((time.clock() - overallTime))
-
-    # while(True):
-    # Capture frame-by-frame
-    # ret, img = cap.read()
-    # img = cv2.imread('family.jpg')
     img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
     # Our operations on the frame come here
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -56,139 +67,49 @@ while (True):
 
     blink_flag = False
 
-    # for (x, y, w, h) in faces:
-    #     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    #     roi_gray = gray[y:y + (3*h/4), x:x + w]
-    #     roi_color = img[y:y + (3*h/4), x:x + w]
-    #     eyes = eye_cascade.detectMultiScale(roi_gray,1.3,4,cv2.cv.CV_HAAR_SCALE_IMAGE,(20,20))
-    #     eyes_detected = 0
-    #     for (ex, ey, ew, eh) in eyes:
-    #         eyes_detected += 1
-    #         eye_img = roi_gray[ey:ey+eh, ex:ex+ew]
-    #         eye_img_color = roi_color[ey:ey+eh, ex:ex+ew]
-    #         # cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-    #         # # hist = cv2.calcHist(eye_img,[0],None,[256],[0,256])
-    #         # circles = cv2.HoughCircles(eye_img,cv2.cv.CV_HOUGH_GRADIENT,1,120,
-    #         #                 param1=50,param2=30,minRadius=5,maxRadius=30)
-    #         # # circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 75)
-    #         # if circles is not None:
-    #         #     # print len(circles) ,"circles found on 1 eye"
-    #         # # convert the (x, y) coordinates and radius of the circles to integers
-    #         #     circles = np.round(circles[0, :]).astype("int")
-
-    #         #     # loop over the (x, y) coordinates and radius of the circles
-    #         #     for (x, y, r) in circles:
-    #         #         # draw the circle in the output image, then draw a rectangle
-    #         #         # corresponding to the center of the circle
-    #         #         cv2.circle(eye_img_color, (x, y), r, (0, 0, 255), 4)
-    #         #         # cv2.rectangle(img, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-    #         if (eyes_detected==2):
-    #             break
     frame = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-    detected = eye_cascade.detectMultiScale(frame, 1.3, 5)
+        # print frame.shape
 
-    #faces = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    #detected2 = faces.detectMultiScale(frameDBW, 1.3, 5)
-    
-    pupilFrame = frame
-    pupilO = frame
-    windowClose = np.ones((5,5),np.uint8)
-    windowOpen = np.ones((2,2),np.uint8)
-    windowErode = np.ones((2,2),np.uint8)
-
-    #draw square
-    for (x,y,w,h) in detected:
-        cv2.rectangle(frame, (x,y), ((x+w),(y+h)), (0,0,255),1) 
-        cv2.line(frame, (x,y), ((x+w,y+h)), (0,0,255),1)
-        cv2.line(frame, (x+w,y), ((x,y+h)), (0,0,255),1)
-        pupilFrame = cv2.equalizeHist(frame[y+(h*.25):(y+h), x:(x+w)])
-        pupilO = pupilFrame
-        ret, pupilFrame = cv2.threshold(pupilFrame,55,255,cv2.THRESH_BINARY)        #50 ..nothin 70 is better
-        pupilFrame = cv2.morphologyEx(pupilFrame, cv2.MORPH_CLOSE, windowClose)
-        pupilFrame = cv2.morphologyEx(pupilFrame, cv2.MORPH_ERODE, windowErode)
-        pupilFrame = cv2.morphologyEx(pupilFrame, cv2.MORPH_OPEN, windowOpen)
-
-        #so above we do image processing to get the pupil..
-        #now we find the biggest blob and get the centriod
-        
-        threshold = cv2.inRange(pupilFrame,250,255)     #get the blobs
-        contours, hierarchy = cv2.findContours(threshold,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-        
-        #if there are 3 or more blobs, delete the biggest and delete the left most for the right eye
-        #if there are 2 blob, take the second largest
-        #if there are 1 or less blobs, do nothing
-        
-        if len(contours) >= 2:
-            #find biggest blob
-            maxArea = 0
-            MAindex = 0         #to get the unwanted frame 
-            distanceX = []      #delete the left most (for right eye)
-            currentIndex = 0 
-            for cnt in contours:
-                area = cv2.contourArea(cnt)
-                center = cv2.moments(cnt)
-                if (center['m00']!=0):
-                    cx,cy = int(center['m10']/center['m00']), int(center['m01']/center['m00'])
-                else:
-                    cx=0
-                    cy=0
-                distanceX.append(cx)    
-                if area > maxArea:
-                    maxArea = area
-                    MAindex = currentIndex
-                currentIndex = currentIndex + 1
-    
-            del contours[MAindex]       #remove the picture frame contour
-            del distanceX[MAindex]
-        
-        eye = 'right'
-
-        if len(contours) >= 2:      #delete the left most blob for right eye
-            if eye == 'right':
-                edgeOfEye = distanceX.index(min(distanceX))
-            else:
-                edgeOfEye = distanceX.index(max(distanceX)) 
-            del contours[edgeOfEye]
-            del distanceX[edgeOfEye]
-
-        if len(contours) >= 1:      #get largest blob
-            maxArea = 0
-            for cnt in contours:
-                area = cv2.contourArea(cnt)
-                if area > maxArea:
-                    maxArea = area
-                    largeBlob = cnt
-                
-        if len(largeBlob) > 0:  
-            center = cv2.moments(largeBlob)
-            if (center['m00']!=0):
-                cx,cy = int(center['m10']/center['m00']), int(center['m01']/center['m00'])
-            else:
-                cx=0
-                cy=0
-            # cx,cy = int(center['m10']/center['m00']), int(center['m01']/center['m00'])
-            cv2.circle(pupilO,(cx,cy),5,255,-1)
-    
-    if (len(detected)==0):
-        print "Blinking!"
-    if len(detected) == 0:
-        if eyes_flag2>0:
-            blink_flag = True
-            eyes_flag2 = 0
-        else:
-            eyes_flag2+=1
-    else:
-        blink_flag = False
-        eyes_flag2 = 0
-            # limit to 1
-
-
-    faces = face_cascade.detectMultiScale(gray, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20, 20))
+    faces = face_cascade.detectMultiScale(gray, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, minSize=(50, 50))
     for (x, y, w, h) in faces:
+
+        eye_frame = gray[(y+h/5):(y+h/2), x:x+w]
+
+        detected = eyes.detectMultiScale(eye_frame, minNeighbors=4,scaleFactor=1.05, minSize=(7,7))
+        if len(detected) == 0:
+            if eyes_flag2>0:
+                blink_flag = True
+                eyes_flag2 = 0
+            else:
+                eyes_flag2+=1
+        else:
+            eyes_flag2 = 0
+            if (blink_bool==True and recorded_time_blink==False):
+                # opened eyes, so record the time they were closed
+                timeSpent = (time.clock() - blink_start)
+                print "Blink was:",timeSpent," seconds long"
+                if (len(blinkLengthArr)<10):
+                    blinkLengthArr.append(timeSpent)
+                else:
+                    blinkLengthArr = blinkLengthArr[1:]
+                    blinkLengthArr.append(timeSpent)
+                recorded_time_blink = True
+
+                #display average blinking time
+                last_minute_blinking = blinkLengthArr
+                print last_minute_blinking
+                blink_length_avg = -1
+                if not(last_minute_blinking==[]):
+                    blink_length_avg = sum(last_minute_blinking)/(float(len(last_minute_blinking)))
+                    print "Average blinking time is ",blink_length_avg
+            blink_flag = False
+
+        # cv2.imshow("eyes",eye_frame)
+
+
         roi_gray_mouth = gray[(y+h/2):y+h, x:x + w]
         roi_color_mouth = img[(y+h/2):y+h, x:x + w]
-    # roi_gray_mouth = gray
-    # roi_color_mouth = img
+        cv2.imshow("mouth",roi_gray_mouth)
 
         mouth = mouth_cascade.detectMultiScale(roi_gray_mouth,1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20, 20))
         # print "Length:",len(mouth)
@@ -198,19 +119,7 @@ while (True):
             mouth_detected = True
             cv2.rectangle(roi_color_mouth, (mx, my), (mx + mw, my + mh), (255, 0, 255), 2)
             roi_gray_mouth = gray[my:my+mh*2, mx:mx + 2*mw]
-            # circles = cv2.HoughCircles(roi_gray_mouth,cv2.cv.CV_HOUGH_GRADIENT,1,120,
-            #         param1=50,param2=20,minRadius=10,maxRadius=300)
-            # if circles is not None:
-            #     # print len(circles) ,"circles found on 1 mouth"
-            # # convert the (x, y) coordinates and radius of the circles to integers
-            #     circles = np.round(circles[0, :]).astype("int")
-
-            #     # loop over the (x, y) coordinates and radius of the circles
-            #     for (x, y, r) in circles:
-            #         # draw the circle in the output image, then draw a rectangle
-            #         # corresponding to the center of the circle
-            #         cv2.circle(roi_color_mouth, (x, y), r, (0, 0, 255), 4)
-                    # cv2.rectangle(img, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+           
             if mouth_detected:
                 break
 
@@ -227,9 +136,10 @@ while (True):
         break
 
 
-    if (time.clock() - blink_start) > 0.4:
+    if (time.clock() - blink_start) > 1:
         if (blink_flag == False):
             blink_bool = False
+            recorded_time_blink = False
 
     if (time.clock() - yawn_start) > 5.0:
         if yawn_flag == False:
@@ -242,8 +152,17 @@ while (True):
             blink_start = time.clock()
             blink_bool = True
             blink_count += 1
-            print "Detect blink, total:", blink_count
-            print "Blink Frequency: ", blink_frequency, " in ", (time.clock() - overallTime), " sec"
+
+            #time - lastBlink = time since last blink
+            if (len(blinkArr)<50):
+                blinkArr.append(time.clock())
+            else:
+                blinkArr = blinkArr[1:]
+                blinkArr.append(time.clock())
+            # print "Blink Array: ",blinkArr
+            blinkRatio = getRate(blinkArr)
+            print "Blink Ratio: ",blinkRatio, "blinks per minute"
+
         # else:
         #     print "detect blink but disabled"
         # get the time spent
@@ -257,38 +176,32 @@ while (True):
             print "Detect yawn, total:", yawn_count
             print "Yawn Frequency: ", yawn_frequency, " in ", (time.clock() - overallTime), " sec"
 
-            # print "detected yawn but disabled"
 
         # if less than 20 ms, do nothing
     timeElapsed = int(time.clock() - timeToSend)
     # print "Elapsed time:",timeElapsed
-    if (timeElapsed>0 and (timeElapsed%20==0) and sent==False):
-        print "\n\n\n\n ----- SENDING DATA ------- \n"
-        payload = {'yawnRate':yawn_frequency , 'blinkRate':blink_frequency}
-        url = "https://mhealthhelloworld-bpeynetti.c9users.io/insert.php"
-        r = requests.post(url,data=payload)
-        #print r.text
+    if (timeElapsed>0 and (timeElapsed%30==0) and sent==False):
+        print "\n\n\n\n ----- SAVING DATA ------- \n"
+        # payload = {'yawnRate':yawn_frequency , 'blinkRate':blink_frequency}
+        # url = "https://mhealthhelloworld-bpeynetti.c9users.io/insert.php"
+        # r = requests.post(url,data=payload)
+        txt = str(blinkRatio)+','+str(blink_length_avg)+'\n'
+        file.write(txt)
         sent = True
-    if (timeElapsed%20==1):
+    if (timeElapsed%30==1):
         sent = False
-    # if (int(time.clock() - timeToSend)%10)==0:
-    #     print "\n\n ------ SENDING DATA --------"
-    #     payload = {'yawnRate':yawn_frequency , 'blinkRate':blink_frequency}
-    #     url = "https://mhealthhelloworld-bpeynetti.c9users.io/insert.php"
-    #     r = requests.post(url,data=payload)
-    #     print r.text
-    # print "Found ",len(eyes)," eyes!"
-    # print "Found ",len(eyes)," eyes!"
-    # print "Found ",len(faces)," faces!"
-    # Display the resulting frame
-    # cv2.imshow("pupil",pupilO)
+
+
     cv2.imshow('Video', img)
-    if (blink_count == 50):
+    if (blink_count == 200):
         break
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break  # time.sleep(1)
-# cv2.waitKey(0)	
 
+print "WRITING DATA"
+
+file.write(txt)
+file.close()
 # When everything done, release the capture
 # cap.release()
 cv2.destroyAllWindows()
